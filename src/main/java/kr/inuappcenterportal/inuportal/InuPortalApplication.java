ec3d06e21dd38feb73e3d97447a01293104908a9
hen715 <130031561+hen715@users.noreply.github.com>
2024-01-18 23:54:03 +0900

    init
    init

---
+++ src/main/java/kr/inuappcenterportal/inuportal/InuPortalApplication.java
@@ -0,0 +1,13 @@
+package kr.inuappcenterportal.inuportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InuPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InuPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/inuappcenterportal/inuportal/InuPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.inuappcenterportal.inuportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InuPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
