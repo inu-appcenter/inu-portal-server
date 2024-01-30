10bc4ee0c2d59f67ed68561ee2997a4531187ce8
hen715 <hen715@naver.com>
2024-01-30 23:57:48 +0900

    [Refactor] CORS설정

--- src/main/java/kr/inuappcenterportal/inuportal/InuPortalApplication.java
+++ src/main/java/kr/inuappcenterportal/inuportal/InuPortalApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @OpenAPIDefinition(
         servers = {
-                @Server(url = "http://portal.inuappcenter.kr/", description = "Server URL"),
+                @Server(url = "https://portal.inuappcenter.kr/", description = "Server URL"),
                 @Server(url = "http://localhost:8080",description = "Local URL")
         }
 )
