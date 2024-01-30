30fe57493462ef0af797e152d1b2c9de651e41ce
hen715 <hen715@naver.com>
2024-01-31 00:13:42 +0900

    [Refactor] 명세 추가

--- src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
@@ -1,5 +1,10 @@
 package kr.inuappcenterportal.inuportal.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import kr.inuappcenterportal.inuportal.config.TokenProvider;
 import kr.inuappcenterportal.inuportal.dto.MemberLoginDto;
@@ -21,7 +26,10 @@ public class MemberController {
     private final TokenProvider tokenProvider;
     private final MemberService memberService;
 
-
+    @Operation(summary = "회원 가입",description = "바디에 {email,password}을 json 형식으로 보내주세요.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",description = "회원가입성공")
+    })
     @PostMapping("")
     public ResponseEntity<?> join(@Valid @RequestBody MemberSaveDto memberSaveDto){
         Long id = memberService.join(memberSaveDto);
@@ -29,20 +37,35 @@ public class MemberController {
         return new ResponseEntity<>(new ResponseDto(id,"회원가입성공"), HttpStatus.CREATED);
     }
 
+
+    @Parameter(name = "Auth",description = "로그인 후 발급 받은 토큰",required = true,in = ParameterIn.HEADER)
+    @Operation(summary = "회원 정보 수정",description = "url 헤더에 Auth 토큰,바디에 {password}을 json 형식으로 보내주세요.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "회원정보수정성공")
+    })
     @PutMapping("")
-    public ResponseEntity<?> update(@RequestHeader("auth") String token, @RequestParam("password") String password){
+    public ResponseEntity<?> update(@RequestHeader("Auth") String token, @RequestBody String password){
         Long id = Long.valueOf(tokenProvider.getUsername(token));
         Long member_id = memberService.changePassword(id, password);
         return new ResponseEntity<>(new ResponseDto(member_id,"비밀번호 변경 성공"),HttpStatus.OK);
     }
 
+    @Parameter(name = "Auth",description = "로그인 후 발급 받은 토큰",required = true,in = ParameterIn.HEADER)
+    @Operation(summary = "회원 삭제",description = "url 헤더에 Auth 토큰을 담아 보내주세요")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204",description = "회원삭제성공")
+    })
     @DeleteMapping("")
-    public ResponseEntity<?> delete(@RequestHeader("auth") String token){
+    public ResponseEntity<?> delete(@RequestHeader("Auth") String token){
         Long id = Long.valueOf(tokenProvider.getUsername(token));
         memberService.delete(id);
         return new ResponseEntity<>(new ResponseDto(id,"회원삭제성공"), HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "로그인 시 토근이 발급됩니다",description = "바디에 {email,password}을 json 형식으로 보내주세요.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "로그인 성공")
+    })
     @PostMapping("/login")
     public ResponseEntity<?> Login(@Valid @RequestBody MemberLoginDto memberLoginDto){
         log.info("로그인 호출");
@@ -50,8 +73,13 @@ public class MemberController {
         return new ResponseEntity<>(token,HttpStatus.OK);
     }
 
+    @Parameter(name = "Auth",description = "로그인 후 발급 받은 토큰",required = true,in = ParameterIn.HEADER)
+    @Operation(summary = "회원 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "회원가져오기성공")
+    })
     @GetMapping("")
-    public ResponseEntity<?> getMember(@RequestHeader("auth") String token){
+    public ResponseEntity<?> getMember(@RequestHeader("Auth") String token){
         Long id = Long.valueOf(tokenProvider.getUsername(token));
         return new ResponseEntity<>(memberService.getMember(id),HttpStatus.OK);
     }
