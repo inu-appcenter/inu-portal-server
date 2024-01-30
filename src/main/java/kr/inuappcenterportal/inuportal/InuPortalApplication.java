56df110db1ae0a79cef74004027cb4b0893c637b
hen715 <hen715@naver.com>
2024-01-30 19:22:13 +0900

    [Refactor] db커넥션 수정

--- src/main/java/kr/inuappcenterportal/inuportal/InuPortalApplication.java
+++ src/main/java/kr/inuappcenterportal/inuportal/InuPortalApplication.java
@@ -1,9 +1,16 @@
 package kr.inuappcenterportal.inuportal;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://portal.inuappcenter.kr/", description = "Server URL"),
+                @Server(url = "http://localhost:8080",description = "Local URL")
+        }
+)
 @EnableJpaAuditing
 @SpringBootApplication
 public class InuPortalApplication {
