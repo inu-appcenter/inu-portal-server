ce236d687817824de7bf5785aec250da47961d1e
hen715 <hen715@naver.com>
2024-02-01 18:44:28 +0900

    [Feat] 좋아요,싫어요 중복 기능 추가

--- src/main/java/kr/inuappcenterportal/inuportal/repository/DisLikeRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/repository/DisLikeRepository.java
@@ -1,7 +1,10 @@
 package kr.inuappcenterportal.inuportal.repository;
 
 import kr.inuappcenterportal.inuportal.domain.DisLike;
+import kr.inuappcenterportal.inuportal.domain.Member;
+import kr.inuappcenterportal.inuportal.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DisLikeRepository extends JpaRepository<DisLike,Long> {
+    boolean existsByMemberAndPost(Member member, Post post);
 }
--- src/main/java/kr/inuappcenterportal/inuportal/repository/LikeRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/repository/LikeRepository.java
@@ -1,7 +1,11 @@
 package kr.inuappcenterportal.inuportal.repository;
 
 import kr.inuappcenterportal.inuportal.domain.Good;
+import kr.inuappcenterportal.inuportal.domain.Member;
+import kr.inuappcenterportal.inuportal.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface LikeRepository extends JpaRepository<Good,Long> {
+    boolean existsByMemberAndPost(Member member, Post post);
 }
