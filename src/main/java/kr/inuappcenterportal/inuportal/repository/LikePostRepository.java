e330de9cfa471992dfb5b74df424b9240e6f3aaa
hen715 <hen715@naver.com>
2024-02-16 00:02:32 +0900

    [Feat] 멤버로 좋아요 가져오기 기능 추가

--- src/main/java/kr/inuappcenterportal/inuportal/repository/LikePostRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/repository/LikePostRepository.java
@@ -5,10 +5,12 @@ import kr.inuappcenterportal.inuportal.domain.Member;
 import kr.inuappcenterportal.inuportal.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface LikePostRepository extends JpaRepository<PostLike,Long> {
     boolean existsByMemberAndPost(Member member, Post post);
     Optional<PostLike> findByMemberAndPost(Member member, Post post);
+    List<PostLike> findAllByMember(Member member);
 }
