da9ffc598b2edfc578bd8e22cd47ec492015c49d
hen715 <hen715@naver.com>
2024-02-16 00:02:42 +0900

    [Feat] 좋아요한 글 가져오기 추가

--- src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
@@ -132,7 +132,19 @@ public class MemberController {
     public ResponseEntity<ResponseDto<List<PostListResponseDto>>> getAllScrap(HttpServletRequest httpServletRequest){
         Long id = Long.valueOf(tokenProvider.getUsername(httpServletRequest.getHeader("Auth")));
         log.info("회원이 스크랩한 모든 글 가져오기 호출 id:{}",id);
-        return new ResponseEntity<>(new ResponseDto<>(postService.getAllScraps(id),"회원이 스크랩한 모든 게시글 가져오기 성공"),HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDto<>(postService.getScrapsByMember(id),"회원이 스크랩한 모든 게시글 가져오기 성공"),HttpStatus.OK);
+    }
+
+    @Operation(summary = "회원이 좋아요한 모든 글 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "회원이 스크랩한 모든 글 가져오기성공",content = @Content(schema = @Schema(implementation = PostListResponseDto.class)))
+            ,@ApiResponse(responseCode = "404",description = "존재하지 않는 회원입니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
+    @GetMapping("/likes")
+    public ResponseEntity<ResponseDto<List<PostListResponseDto>>> getAllLike(HttpServletRequest httpServletRequest){
+        Long id = Long.valueOf(tokenProvider.getUsername(httpServletRequest.getHeader("Auth")));
+        log.info("회원이 좋아요한 모든 글 가져오기 호출 id:{}",id);
+        return new ResponseEntity<>(new ResponseDto<>(postService.getLikeByMember(id),"회원이 좋아요한 모든 게시글 가져오기 성공"),HttpStatus.OK);
     }
 
     @Operation(summary = "회원이 작성한 모든 댓글 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요.")
@@ -146,4 +158,6 @@ public class MemberController {
         log.info("회원이 작성한 모든 댓글 호출 id:{}",id);
         return new ResponseEntity<>(new ResponseDto<>(replyService.getReplyByMember(id),"회원이 작성한 모든 댓글 가져오기 성공"),HttpStatus.OK);
     }
+
+
 }
