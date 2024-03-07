5ac10742aa8f43815545c2baadeb1a6cd18982fd
hen715 <hen715@naver.com>
2024-03-07 16:54:11 +0900

    [Feat] 내가 쓴 댓글 정렬 추가

--- src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
@@ -135,8 +135,8 @@ public class MemberController {
     @Operation(summary = "회원이 스크랩한 모든 글 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요. 정렬기준 sort(공백일 시 스크랩 한 순서,like,date)를 보내주세요.")
     @ApiResponses({
             @ApiResponse(responseCode = "200",description = "회원이 스크랩한 모든 글 가져오기성공",content = @Content(schema = @Schema(implementation = PostListResponseDto.class)))
-            ,@ApiResponse(responseCode = "404",description = "존재하지 않는 회원입니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class))),
-            @ApiResponse(responseCode = "400",description = "정렬의 기준값이 올바르지 않습니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+            ,@ApiResponse(responseCode = "404",description = "존재하지 않는 회원입니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+            ,@ApiResponse(responseCode = "400",description = "정렬의 기준값이 올바르지 않습니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
     })
     @GetMapping("/scraps")
     public ResponseEntity<ResponseDto<List<PostListResponseDto>>> getAllScrap(HttpServletRequest httpServletRequest, @RequestParam(required = false) String sort){
@@ -157,16 +157,17 @@ public class MemberController {
         return new ResponseEntity<>(new ResponseDto<>(postService.getLikeByMember(id),"회원이 좋아요한 모든 게시글 가져오기 성공"),HttpStatus.OK);
     }
 
-    @Operation(summary = "회원이 작성한 모든 댓글 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요.")
+    @Operation(summary = "회원이 작성한 모든 댓글 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요. 정렬기준은 sort(공백 시 최신순, like) 둘 중 하나로 보내주세요.")
     @ApiResponses({
             @ApiResponse(responseCode = "200",description = "회원이 스크랩한 모든 글 가져오기성공",content = @Content(schema = @Schema(implementation = PostListResponseDto.class)))
             ,@ApiResponse(responseCode = "404",description = "존재하지 않는 회원입니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+            ,@ApiResponse(responseCode = "400",description = "정렬의 기준값이 올바르지 않습니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
     })
     @GetMapping("/replies")
-    public ResponseEntity<ResponseDto<List<ReplyListResponseDto>>> getAllReply(HttpServletRequest httpServletRequest){
+    public ResponseEntity<ResponseDto<List<ReplyListResponseDto>>> getAllReply(HttpServletRequest httpServletRequest, @RequestParam(required = false) String sort){
         Long id = Long.valueOf(tokenProvider.getUsername(httpServletRequest.getHeader("Auth")));
         log.info("회원이 작성한 모든 댓글 호출 id:{}",id);
-        return new ResponseEntity<>(new ResponseDto<>(replyService.getReplyByMember(id),"회원이 작성한 모든 댓글 가져오기 성공"),HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDto<>(replyService.getReplyByMember(id,sort),"회원이 작성한 모든 댓글 가져오기 성공"),HttpStatus.OK);
     }
 
 
