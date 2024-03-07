4f5dad89d6be0dfb59203704db7022fd451ebce5
hen715 <hen715@naver.com>
2024-03-07 16:25:35 +0900

    [Refactor] 스웨거 설명 수정

--- src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/controller/MemberController.java
@@ -134,7 +134,7 @@ public class MemberController {
         return new ResponseEntity<>(new ResponseDto<>(postService.getPostByMember(id),"회원이 작성한 모든 게시글 가져오기 성공"),HttpStatus.OK);
     }
 
-    @Operation(summary = "회원이 스크랩한 모든 글 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요")
+    @Operation(summary = "회원이 스크랩한 모든 글 가져오기",description = "url 헤더에 Auth 토큰을 담아 보내주세요. 정렬기준 sort(공백일 시 스크랩 한 순서,like,date)를 보내주세요. ")
     @ApiResponses({
             @ApiResponse(responseCode = "200",description = "회원이 스크랩한 모든 글 가져오기성공",content = @Content(schema = @Schema(implementation = PostListResponseDto.class)))
             ,@ApiResponse(responseCode = "404",description = "존재하지 않는 회원입니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
