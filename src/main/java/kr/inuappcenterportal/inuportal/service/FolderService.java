5c4e19118ff03aa1d981166012a4d53a5affd9b4
hen715 <hen715@naver.com>
2024-03-08 16:20:43 +0900

    [Refactor] 리스트 방식 수정

--- src/main/java/kr/inuappcenterportal/inuportal/service/FolderService.java
+++ src/main/java/kr/inuappcenterportal/inuportal/service/FolderService.java
@@ -87,7 +87,7 @@ public class FolderService {
     @Transactional(readOnly = true)
     public List<PostListResponseDto> getPostInFolder(Long folderId, String sort) {
         Folder folder = folderRepository.findById(folderId).orElseThrow(() -> new MyNotFoundException(MyErrorCode.FOLDER_NOT_FOUND));
-        List<PostListResponseDto> folderDto = folderPostRepository.findAllByFolder(folder).stream().map(file -> postService.getPostListResponseDto(file.getPost())).sorted(Comparator.comparing(PostListResponseDto::getId).reversed()).toList();
+        List<PostListResponseDto> folderDto = folderPostRepository.findAllByFolder(folder).stream().map(file -> postService.getPostListResponseDto(file.getPost())).sorted(Comparator.comparing(PostListResponseDto::getId).reversed()).collect(Collectors.toList());
         return postService.getPostListByMember(folderDto,sort);
     }
 
