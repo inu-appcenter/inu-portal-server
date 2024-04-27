efe936e1b73802cf4e6a27bd9cf13ffc66378ea0
hen715 <hen715@naver.com>
2024-04-27 21:17:08 +0900

    [Feat] 이중데이터 베이스 서버 테스트

---
+++ src/main/java/kr/inuappcenterportal/inuportal/oracleRepository/SchoolLoginRepository.java
@@ -0,0 +1,32 @@
+package kr.inuappcenterportal.inuportal.oracleRepository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+
+@Component
+@Slf4j
+public class SchoolLoginRepository {
+
+    @Autowired
+    @Qualifier("oracleJdbc")
+    private JdbcTemplate jdbcTemplate;
+
+
+    public boolean loginCheck(String username, String password) {
+        String sql = "SELECT F_LOGIN_CHECK(?, ?) FROM DUAL";
+        try {
+            String result = jdbcTemplate.queryForObject(sql, String.class, username, password);
+            return "1".equals(result);
+        } catch (Exception e) {
+            log.info("데이터베이스 연결 오류 메시지 : {}",e.getMessage());
+            return false;
+        }
+    }
+}
