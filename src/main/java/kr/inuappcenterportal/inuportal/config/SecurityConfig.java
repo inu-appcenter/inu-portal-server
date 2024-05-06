2ac455711c3427ab0cb02e743a4779a783f726e0
hen715 <hen715@naver.com>
2024-05-07 03:10:54 +0900

    [Refactor] 횃불이 그림 관리자만 가능하게 설정

--- src/main/java/kr/inuappcenterportal/inuportal/config/SecurityConfig.java
+++ src/main/java/kr/inuappcenterportal/inuportal/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
         httpSecurity
                 .authorizeHttpRequests(auth->auth.requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.POST,"/api/members/**","/api/members").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/fires").hasRole("ADMIN")
                         .requestMatchers("/api/members/all").hasRole("ADMIN")
                         .requestMatchers("/api/members/**","/api/members").hasAnyRole("USER","ADMIN")
                         .requestMatchers(HttpMethod.GET,"/api/posts/**","/api/posts","/api/fires/**","/api/cafeterias","/api/weathers").permitAll()
