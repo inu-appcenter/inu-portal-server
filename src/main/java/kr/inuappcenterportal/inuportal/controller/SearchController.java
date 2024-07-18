35e9ab7522468fe0961aed083accfba01413f1f1
hen715 <hen715@naver.com>
2024-07-18 20:08:58 +0900

    [Refactor] 정적 팩토리 메소드 응답 수정

--- src/main/java/kr/inuappcenterportal/inuportal/controller/SearchController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/controller/SearchController.java
@@ -44,7 +44,7 @@ public class SearchController {
     @GetMapping("")
     public ResponseEntity<ResponseDto<ListResponseDto>> search(@RequestParam @NotBlank(message = "공백일 수 없습니다.") @Size(min = 2,message = "2글자 이상 입력해야 합니다.") String query, @RequestParam(required = false) String sort
     , @RequestParam(required = false,defaultValue = "1") @Min(1) int page){
-        return ResponseEntity.of(ResponseDto.of(postService.searchPost(query,sort,page),"게시글 검색 성공"));
+        return ResponseEntity.ok(ResponseDto.of(postService.searchPost(query,sort,page),"게시글 검색 성공"));
     }
 
     @Operation(summary = "스크랩 게시글 검색",description = "url 헤더에 Auth 토큰, url 파라미터에 검색내용 query , 정렬기준을 sort(date/공백(최신순),like,scrap)를, 페이지(공백일 시 1)를 보내주세요.")
