8aed0e8f1c8bfabd1f18a1e956a0e8d73e2fbd0d
hen715 <hen715@naver.com>
2024-07-23 18:07:02 +0900

    [Refactor] 학교 데이터베이스 관련 문제로 임시 로그인 조치 완료

--- src/main/java/kr/inuappcenterportal/inuportal/service/MemberService.java
+++ src/main/java/kr/inuappcenterportal/inuportal/service/MemberService.java
@@ -98,21 +98,15 @@ public class MemberService {
 
     @Transactional
     public TokenDto schoolLogin(LoginDto loginDto){
-        //서버 관련 문제로 일시적 관리자 로그인 추가
-        if(memberRepository.existsByStudentId(loginDto.getStudentId()) && memberRepository.findByStudentId(loginDto.getStudentId()).get().getRoles().get(0).equals("ROLE_ADMIN")){
-            Member member = memberRepository.findByStudentId(loginDto.getStudentId()).get();
-            return login(member);
+        if (!schoolLoginRepository.loginCheck(loginDto.getStudentId(), loginDto.getPassword())) {
+            throw new MyException(MyErrorCode.STUDENT_LOGIN_ERROR);
         }
-        else {
-            if (!schoolLoginRepository.loginCheck(loginDto.getStudentId(), loginDto.getPassword())) {
-                throw new MyException(MyErrorCode.STUDENT_LOGIN_ERROR);
-            }
-            if (!memberRepository.existsByStudentId(loginDto.getStudentId())) {
-                createMember(loginDto.getStudentId());
-            }
-            Member member = memberRepository.findByStudentId(loginDto.getStudentId()).orElseThrow(() -> new MyException(MyErrorCode.USER_NOT_FOUND));
-            return login(member);
+        if (!memberRepository.existsByStudentId(loginDto.getStudentId())) {
+            createMember(loginDto.getStudentId());
         }
+        Member member = memberRepository.findByStudentId(loginDto.getStudentId()).orElseThrow(() -> new MyException(MyErrorCode.USER_NOT_FOUND));
+        return login(member);
+
     }
     @Transactional
     public void createMember(String studentId){
