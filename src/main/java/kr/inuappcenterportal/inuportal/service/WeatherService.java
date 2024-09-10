49ed1a60e2bb0e8ee4f1f9f365f81bd9456a71bb
hyeseong <hen715@naver.com>
2024-09-10 10:33:23 +0900

    [Refactor] 불필요 로그 제거

--- src/main/java/kr/inuappcenterportal/inuportal/service/WeatherService.java
+++ src/main/java/kr/inuappcenterportal/inuportal/service/WeatherService.java
@@ -251,7 +251,6 @@ public class WeatherService {
                 .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> Mono.just(new MyException(MyErrorCode.WEATHER_REQUEST_ERROR)))
                 .bodyToMono(String.class)
                 .block();
-        log.info(result.toString());
         try {
             JsonObject jsonObject = JsonParser.parseString(result).getAsJsonObject();
             JsonObject body = jsonObject.getAsJsonObject("response").getAsJsonObject("body");
