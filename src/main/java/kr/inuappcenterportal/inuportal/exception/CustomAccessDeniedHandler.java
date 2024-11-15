6fd457771a023f746df9fc065370bfd29c655eaf
hen715 <hen715@naver.com>
2024-11-15 11:22:13 +0900

    [Refactor] 인증 실패 관련 리턴 메시지 타입 변경

--- src/main/java/kr/inuappcenterportal/inuportal/exception/CustomAccessDeniedHandler.java
+++ src/main/java/kr/inuappcenterportal/inuportal/exception/CustomAccessDeniedHandler.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         Gson gson = new Gson();
         String result = gson.toJson((ResponseDto.of(403,"접근 권한이 없는 사용자입니다.")));
         response.setStatus(403);
--- src/main/java/kr/inuappcenterportal/inuportal/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/kr/inuappcenterportal/inuportal/exception/CustomAuthenticationEntryPoint.java
@@ -2,6 +2,7 @@ package kr.inuappcenterportal.inuportal.exception;
 
 import com.google.gson.Gson;
 import kr.inuappcenterportal.inuportal.dto.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,11 +13,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         Gson gson = new Gson();
+        log.info("사람 예외 발생");
         String result = gson.toJson(ResponseDto.of(401,"인증이 실패하였습니다."));
         response.setStatus(401);
         response.getWriter().write(result);
