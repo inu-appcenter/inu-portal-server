92c38c3c4f477f6c44aa40c3d575380262a5f875
hen715 <hen715@naver.com>
2024-11-16 22:06:35 +0900

    [Feat] 회원 정보 수정 유효성 검사 추가

--- src/main/java/kr/inuappcenterportal/inuportal/dto/MemberUpdateNicknameDto.java
+++ src/main/java/kr/inuappcenterportal/inuportal/dto/MemberUpdateNicknameDto.java
@@ -1,6 +1,8 @@
 package kr.inuappcenterportal.inuportal.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,8 @@ public class MemberUpdateNicknameDto {
     private String nickname;
 
     @Schema(description = "횃불이 번호")
+    @Min(1)
+    @Max(12)
     private Long fireId;
 
     @Builder
