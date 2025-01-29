073a0a8c66642326d304674c91a5becf0d60a087
rnwnsgud <rnwnsgud90@naver.com>
2025-01-29 19:37:22 +0900

    분실물 리스트 조회에 content, createDate 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/lostProperty/dto/LostPropertyPreview.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/lostProperty/dto/LostPropertyPreview.java
@@ -1,17 +1,26 @@
 package kr.inuappcenterportal.inuportal.domain.lostProperty.dto;
 
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 public class LostPropertyPreview {
 
     private Long id;
     private String name;
+    private String content;
     private int imageCount;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createdAt;
 
-    public LostPropertyPreview(Long id, String name, int imageCount) {
+    public LostPropertyPreview(Long id, String name, String content, int imageCount, LocalDate createdAt) {
         this.id = id;
         this.name = name;
+        this.content = content;
         this.imageCount = imageCount;
+        this.createdAt = createdAt;
     }
 }
