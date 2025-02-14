3f6b8058047d04e183493aecbd9c3cfe12779b1a
rnwnsgud <rnwnsgud90@naver.com>
2025-02-15 02:14:31 +0900

    [Fix]

--- src/main/java/kr/inuappcenterportal/inuportal/domain/reservation/dto/ReservationCreate.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/reservation/dto/ReservationCreate.java
@@ -1,5 +1,6 @@
 package kr.inuappcenterportal.inuportal.domain.reservation.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -8,9 +9,12 @@ import java.time.LocalDateTime;
 @Getter
 public class ReservationCreate {
 
+    @NotBlank
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDateTime;
+    @NotBlank
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endDateTime;
+    @NotBlank
     private String phoneNumber;
 }
