d074853f584a386ea77096679a1fca4eac90e669
hen715 <hen715@naver.com>
2025-03-05 18:36:25 +0900

    [Refactor] 이미지 캐싱을 위한 아이템 생성 시간 관리 필드 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/item/dto/ItemDetail.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/item/dto/ItemDetail.java
@@ -3,6 +3,10 @@ package kr.inuappcenterportal.inuportal.domain.item.dto;
 import kr.inuappcenterportal.inuportal.domain.item.model.Item;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -12,6 +16,10 @@ public class ItemDetail {
     private String name;
     private int totalQuantity;
     private int deposit;
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private LocalDate createDate;
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    private LocalDateTime modifiedDate;
 
     public static ItemDetail from(Item item) {
         return ItemDetail.builder()
@@ -19,6 +27,8 @@ public class ItemDetail {
                 .name(item.getName())
                 .totalQuantity(item.getTotalQuantity())
                 .deposit(item.getDeposit())
+                .createDate(item.getCreateDate())
+                .modifiedDate(item.getModifiedDate())
                 .build();
     }
 }
