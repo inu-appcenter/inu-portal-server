5e85d26b2b06e54a42edd6d30f2535ec42cf93e8
hen715 <hen715@naver.com>
2025-03-11 21:05:33 +0900

    [Refactor] 학사일정 날자별 정리

--- src/main/java/kr/inuappcenterportal/inuportal/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/schedule/repository/ScheduleRepository.java
@@ -2,14 +2,12 @@ package kr.inuappcenterportal.inuportal.domain.schedule.repository;
 
 import kr.inuappcenterportal.inuportal.domain.schedule.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
-    @Query("SELECT s FROM Schedule s WHERE (YEAR(s.startDate) = :year AND MONTH(s.startDate) = :month) OR (YEAR(s.endDate) = :year AND MONTH(s.endDate) = :month)")
+    @Query("SELECT s FROM Schedule s WHERE (YEAR(s.startDate) = :year AND MONTH(s.startDate) = :month) OR (YEAR(s.endDate) = :year AND MONTH(s.endDate) = :month) ORDER BY s.startDate ASC")
     List<Schedule> findAllByStartDateOrEndDateMonth(int year, int month);
 
     boolean existsByContent(String content);
