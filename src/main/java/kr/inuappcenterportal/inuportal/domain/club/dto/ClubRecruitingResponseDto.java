1ad0ece47f710b958db3d94326465d3c743a4a97
hen715 <hen715@naver.com>
2025-03-18 18:05:05 +0900

    [Feat] 동아리 모집공고 수정 시간 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/club/dto/ClubRecruitingResponseDto.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/club/dto/ClubRecruitingResponseDto.java
@@ -6,6 +6,9 @@ import kr.inuappcenterportal.inuportal.domain.club.model.Club;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Schema(description = "동아리 모집공고 응답Dto")
 @Getter
@@ -15,17 +18,22 @@ public class ClubRecruitingResponseDto {
     private Long imageCount;
     @Schema(description = "모집 공고")
     private String recruit;
+    @Schema(description = "수정일",example = "yyyy-mm-dd")
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    private LocalDateTime modifiedDate;
 
     @Builder
-    private ClubRecruitingResponseDto(Long imageCount, String  recruit){
+    private ClubRecruitingResponseDto(Long imageCount, String  recruit, LocalDateTime modifiedDate){
         this.imageCount = imageCount;
         this.recruit = recruit;
+        this.modifiedDate = modifiedDate;
     }
 
     public static ClubRecruitingResponseDto from(Club club){
         return ClubRecruitingResponseDto.builder()
                 .recruit(club.getRecruit())
                 .imageCount(club.getImageCount())
+                .modifiedDate(club.getModifiedDate())
                 .build();
     }
 }
