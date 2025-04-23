2366f6b9bde326f2293bd8230799f7a29e00bc6a
hen715 <hen715@naver.com>
2025-04-23 18:02:53 +0900

    [Refactor] 테이블 이름 정상화

--- src/main/java/kr/inuappcenterportal/inuportal/domain/firebase/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.inuappcenterportal.inuportal.domain.firebase.repository;
-
-
-import kr.inuappcenterportal.inuportal.domain.firebase.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message,Long> {
-}
