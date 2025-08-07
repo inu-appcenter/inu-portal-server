acc746b619c24bb0ac53cbd4dadba976f68c5f34
d6nggyun <d6nggyun@gmail.com>
2025-08-08 03:43:20 +0900

    feat : 각 학과 공지사항 크롤링 기능

--- src/main/java/kr/inuappcenterportal/inuportal/domain/notice/controller/NoticeController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/notice/controller/NoticeController.java
@@ -8,7 +8,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
+import kr.inuappcenterportal.inuportal.domain.notice.dto.DepartmentNoticeListResponse;
 import kr.inuappcenterportal.inuportal.domain.notice.dto.NoticeListResponseDto;
+import kr.inuappcenterportal.inuportal.domain.notice.enums.Department;
 import kr.inuappcenterportal.inuportal.domain.notice.service.NoticeService;
 import kr.inuappcenterportal.inuportal.domain.post.dto.PostListResponseDto;
 import kr.inuappcenterportal.inuportal.global.dto.ListResponseDto;
@@ -50,4 +52,23 @@ public class NoticeController {
         return ResponseEntity.ok(ResponseDto.of(noticeService.getTop(),"인기 공지 가져오기 성공"));
     }
 
+    @Operation(summary = "학과 별 공지사항 가져오기",description = "url 파라미터에 학과(빈 값 불가), 정렬기준(sort) 을 date/공백(최신순), view 둘 중 하나), 페이지(공백일 시 1)를 보내주세요")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "학과 별 모든 공지사항 가져오기 성공",content = @Content(schema = @Schema(implementation = DepartmentNoticeListResponse.class))),
+            @ApiResponse(responseCode = "400",description = "정렬의 기준값이 올바르지 않습니다.",content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+
+    })
+    @GetMapping("/department")
+    public ResponseEntity<ResponseDto<ListResponseDto<DepartmentNoticeListResponse>>> getDepartmentNotices(
+            @RequestParam Department department,
+            @RequestParam(required = false,defaultValue = "date") String sort,
+            @RequestParam(required = false,defaultValue = "1") @Min(1) int page) {
+        return ResponseEntity.ok(ResponseDto.of(noticeService.getDepartmentNotices(department, sort, page),"해당 학과 모든 공지사항 가져오기 성공"));
+    }
+
+    @GetMapping("/test-crawling")
+    public String testDepartmentNoticeUrl() {
+        noticeService.crawlingDepartmentNotices();
+        return "성공";
+    }
 }
