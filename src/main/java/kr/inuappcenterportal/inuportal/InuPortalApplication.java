fafe3476dc9d03e8d6c3e60b7820d7daaa207405
hjunieee <hinara12@inu.ac.kr>
2026-05-11 14:01:36 +0900

    Fix: JPA Auditing 설정을 활성화하여 create_date와 modified_date가 NULL로 들어가는 문제 수정

--- src/main/java/kr/inuappcenterportal/inuportal/InuPortalApplication.java
+++ src/main/java/kr/inuappcenterportal/inuportal/InuPortalApplication.java
@@ -2,8 +2,10 @@ package kr.inuappcenterportal.inuportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // 추가
 import org.springframework.scheduling.annotation.EnableScheduling; // 추가
 
+@EnableJpaAuditing // 추가
 @EnableScheduling // 추가
 @SpringBootApplication
 public class InuPortalApplication {
