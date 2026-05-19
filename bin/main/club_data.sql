27b4ad55a7b024265551bca4e92ba573d2fd8cc0
hjunieee <hinara12@inu.ac.kr>
2026-05-20 00:44:09 +0900

    fix: StompHandler 구독 경로 파싱 시 하위 경로 포함 시의 예외 버그

---
+++ bin/main/club_data.sql
@@ -0,0 +1,79 @@
+-- 교양학술 분과
+INSERT INTO club (name, category, image_url, url, home_url)
+VALUES
+('멋쟁이사자처럼', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club1.jpg','https://www.inu.ac.kr/inu/888/subview.do','https://likelion.net/'),
+('I''M아임', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club2.jpg','https://www.inu.ac.kr/inu/889/subview.do',null),
+('unexpecTED', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club3.jpg','https://www.inu.ac.kr/inu/890/subview.do','https://www.instagram.com/tedxincheonu'),
+('세치혀', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club4.jpg','https://www.inu.ac.kr/inu/891/subview.do','https://cafe.naver.com/incheondebate'),
+('아르고나우츠', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club5.jpg','https://www.inu.ac.kr/inu/892/subview.do','https://cafe.naver.com/argonauts'),
+('DT(독서토론마당)', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club6.jpg','https://www.inu.ac.kr/inu/893/subview.do',null),
+('EDA', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club7.jpg','https://www.inu.ac.kr/inu/894/subview.do',null),
+('PINCOM', '교양학술', 'https://www.inu.ac.kr/sites/inu/images/sub/club3/img-club8.jpg','https://www.inu.ac.kr/inu/895/subview.do',null);
+
+
+-- 문화 분과
+INSERT INTO club (name, category, image_url, url, home_url) VALUES
+('INUO', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club1.jpg','https://www.inu.ac.kr/inu/866/subview.do','https://cafe.naver.com/inuorchestra'),
+('인스디스', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club2.jpg','https://www.inu.ac.kr/inu/867/subview.do','https://www.instagram.com/insdies_official'),
+('크레퍼스', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club3.jpg','https://www.inu.ac.kr/inu/868/subview.do','https://www.instagram.com/official_crepers'),
+('I.U.D.C,', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club4.jpg','https://www.inu.ac.kr/inu/869/subview.do',null),
+('파이오니아', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club5.jpg','https://www.inu.ac.kr/inu/870/subview.do',null),
+('인인극회', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club6.jpg','https://www.inu.ac.kr/inu/871/subview.do',null),
+('하늬울림', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club7.jpg','https://www.inu.ac.kr/inu/872/subview.do','https://www.instagram.com/hani_classic_guitar'),
+('함성', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club8.jpg','https://www.inu.ac.kr/inu/873/subview.do',null),
+('울림', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club9.jpg','https://www.inu.ac.kr/inu/874/subview.do',null),
+('젊은영상', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club10.jpg','https://www.inu.ac.kr/inu/875/subview.do',null),
+('포크라인', '문화', 'https://www.inu.ac.kr/sites/inu/images/sub/club1/img-club11.jpg','https://www.inu.ac.kr/inu/876/subview.do','https://www.youtube.com/@folkline6166');
+
+
+
+-- 봉사 분과
+INSERT INTO club (name, category, image_url, url, home_url) VALUES
+('초아다솜', '봉사', 'https://www.inu.ac.kr/sites/inu/images/sub/club6/img-club1.jpg','https://www.inu.ac.kr/inu/912/subview.do','https://www.instagram.com/gn_incheon'),
+('뫼골둥지', '봉사', 'https://www.inu.ac.kr/sites/inu/images/sub/club6/img-club2.jpg','https://www.inu.ac.kr/inu/913/subview.do',null),
+('느을사랑', '봉사', 'https://www.inu.ac.kr/sites/inu/images/sub/club6/img-club3.jpg','https://www.inu.ac.kr/inu/914/subview.do','https://www.instagram.com/neueulsarang_inu'),
+('로타렉트', '봉사', 'https://www.inu.ac.kr/sites/inu/images/sub/club6/img-club4.jpg','https://www.inu.ac.kr/inu/915/subview.do',null);
+
+
+
+
+-- 종교 분과
+INSERT INTO club (name, category, image_url, url, home_url) VALUES
+('CFM', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club1.jpg','https://www.inu.ac.kr/inu/904/subview.do',null),
+('JDM', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club2.jpg','https://www.inu.ac.kr/inu/905/subview.do',null),
+('IVF', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club3.jpg','https://www.inu.ac.kr/inu/906/subview.do','https://www.ivf.or.kr/'),
+('CMI', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club4.jpg','https://www.inu.ac.kr/inu/907/subview.do',null),
+('CCC', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club5.jpg','https://www.inu.ac.kr/inu/908/subview.do','https://www.instagram.com/inu_ccc'),
+('카톨릭학생회', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club6.jpg','https://www.inu.ac.kr/inu/909/subview.do',null),
+('예수전도단(YWAM)', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club7.jpg','https://www.inu.ac.kr/inu/910/subview.do','https://www.ywamincheon.org/'),
+('JOY선교회', '종교', 'https://www.inu.ac.kr/sites/inu/images/sub/club5/img-club8.jpg','https://www.inu.ac.kr/inu/911/subview.do','https://www.instagram.com/joymission_inu');
+
+
+
+-- 체육 분과
+INSERT INTO club (name, category, image_url, url, home_url) VALUES
+('PANG', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club1.jpg', 'https://www.inu.ac.kr/inu/877/subview.do','https://www.instagram.com/inu_squash'),
+('INU W FC', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club2.jpg', 'https://www.inu.ac.kr/inu/878/subview.do','https://www.facebook.com/inuonly1'),
+('싸우라비', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club3.jpg', 'https://www.inu.ac.kr/inu/879/subview.do','https://www.instagram.com/ssawoorabi'),
+('다크호스', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club4.jpg', 'https://www.inu.ac.kr/inu/880/subview.do','https://www.instagram.com/darkhorse_inu'),
+('퍼펙트', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club5.jpg', 'https://www.inu.ac.kr/inu/881/subview.do',null),
+('바이킹', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club6.jpg', 'https://www.inu.ac.kr/inu/882/subview.do','https://cafe.daum.net/qkdlzld'),
+('돌핀', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club7.jpg', 'https://www.inu.ac.kr/inu/883/subview.do',null),
+('UITC', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/UITC.jpg', 'https://www.inu.ac.kr/inu/884/subview.do',null),
+('산악부 UIAC', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club9.jpg', 'https://www.inu.ac.kr/inu/885/subview.do','https://www.instagram.com/inu_uiac'),
+('효월검우회', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club10.jpg', 'https://www.inu.ac.kr/inu/887/subview.do',null),
+('BOSS', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-act11-1.jpg', 'https://www.inu.ac.kr/inu/11443/subview.do','https://www.instagram.com/inu.boss'),
+('INU START W', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club12.jpg', 'https://www.inu.ac.kr/inu/11444/subview.do',null),
+('페더스', '체육', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club8.jpg', 'https://www.inu.ac.kr/inu/13487/subview.do',null);
+
+-- 취미·전시 분과
+INSERT INTO club (name, category, image_url, url, home_url) VALUES
+('보.인.다', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club1.jpg','https://www.inu.ac.kr/inu/896/subview.do','https://www.facebook.com/INUboard/'),
+('인유공방', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club2.jpg','https://www.inu.ac.kr/inu/897/subview.do','https://www.instagram.com/inu_gongbang'),
+('별천지', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club3.jpg','https://www.inu.ac.kr/inu/898/subview.do',null),
+('기우회', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club4.jpg','https://www.inu.ac.kr/inu/899/subview.do',null),
+('셔플', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club5.jpg','https://www.inu.ac.kr/inu/900/subview.do',null),
+('유스호스텔', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club6.jpg','https://www.inu.ac.kr/inu/901/subview.do',null),
+('하양검정', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club7.jpg','https://www.inu.ac.kr/inu/902/subview.do',null),
+('한아랑', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club4/img-club8.jpg','https://www.inu.ac.kr/inu/903/subview.do',null),
+('Cookinu', '취미·전시', 'https://www.inu.ac.kr/sites/inu/images/sub/club2/img-club13.jpg','https://www.inu.ac.kr/inu/11447/subview.do',null);
