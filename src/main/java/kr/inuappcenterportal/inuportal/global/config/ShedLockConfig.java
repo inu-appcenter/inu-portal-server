13780f7ac96f47c5aa7a82ad1a3f794c6ab1f8ec
d6nggyun <d6nggyun@gmail.com>
2026-05-20 02:14:36 +0900

    feat: 분산 락에 의해 차단된 스케줄러 중복 실행 횟수 가시화를 위해 LockProvider 메트릭 래퍼 추가

--- src/main/java/kr/inuappcenterportal/inuportal/global/config/ShedLockConfig.java
+++ src/main/java/kr/inuappcenterportal/inuportal/global/config/ShedLockConfig.java
@@ -1,17 +1,44 @@
 package kr.inuappcenterportal.inuportal.global.config;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.core.SimpleLock;
 import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.util.Optional;
 
 @Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
 public class ShedLockConfig {
 
     @Bean
-    public RedisLockProvider lockProvider(RedisConnectionFactory connectionFactory) {
+    public RedisLockProvider redisLockProvider(RedisConnectionFactory connectionFactory) {
         return new RedisLockProvider(connectionFactory, "inu-portal");
+    }
+
+    @Bean
+    @Primary
+    public LockProvider meteredLockProvider(RedisLockProvider delegate, MeterRegistry registry) {
+        return new LockProvider() {
+            @Override
+            public Optional<SimpleLock> lock(LockConfiguration config) {
+                Optional<SimpleLock> result = delegate.lock(config);
+                if (result.isEmpty()) {
+                    Counter.builder("intip_scheduler_executions_total")
+                            .tag("name", config.getName())
+                            .tag("result", "skipped")
+                            .register(registry)
+                            .increment();
+                }
+                return result;
+            }
+        };
     }
 }
