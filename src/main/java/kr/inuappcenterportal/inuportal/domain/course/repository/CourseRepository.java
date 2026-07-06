94e98e389e9cb04bc0e875afeb3c9fba20cf41a7
siillvergun <duejql03@naver.com>
2026-07-06 14:53:40 +0900

    feat: 강의 관련 repository 개발

--- src/main/java/kr/inuappcenterportal/inuportal/domain/course/repository/CourseRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/repository/CourseRepository.java
@@ -1,7 +1,18 @@
 package kr.inuappcenterportal.inuportal.domain.course.repository;
 
 import kr.inuappcenterportal.inuportal.domain.course.model.Course;
+import kr.inuappcenterportal.inuportal.domain.department.enums.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    Optional<Course> findByTitleAndDepartment(String title, Department department);
+
+    List<Course> findAllByDepartment(Department department);
+
+    List<Course> findAllByActiveTrue();
+
+    List<Course> findAllByDepartmentAndActiveTrue(Department department);
 }
