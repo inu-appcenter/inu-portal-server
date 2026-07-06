61307085f561ba8c1c31ba15bb2039ede57efc41
siillvergun <duejql03@naver.com>
2026-07-06 18:17:36 +0900

    feat: 동기화 컨트롤러를 관리자 전용으로 분리

--- src/main/java/kr/inuappcenterportal/inuportal/domain/course/scheduler/CourseScheduler.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/scheduler/CourseScheduler.java
@@ -1,22 +1,26 @@
-package kr.inuappcenterportal.inuportal.domain.course.scheduller;
+package kr.inuappcenterportal.inuportal.domain.course.scheduler;
 
 import kr.inuappcenterportal.inuportal.domain.course.service.CourseCrawlerService;
-import kr.inuappcenterportal.inuportal.domain.course.service.CourseService;
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class CourseScheduller {
+public class CourseScheduler {
 
-    private final CourseService courseService;
     private final CourseCrawlerService courseCrawlerService;
 
     /**
      * 매월 1일 오전 4시에 동기화 메서드 실행
      */
     @Scheduled(cron = "0 0 4 1 * *")
+    @SchedulerLock(
+            name = "course-base-sync",
+            lockAtMostFor = "PT30M",
+            lockAtLeastFor = "PT1M"
+    )
     public void syncBaseCourses() {
         courseCrawlerService.syncBaseCourses();
     }
--- src/main/java/kr/inuappcenterportal/inuportal/domain/semester/controller/SemesterController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/semester/controller/SemesterController.java
@@ -2,9 +2,11 @@ package kr.inuappcenterportal.inuportal.domain.semester.controller;
 
 import kr.inuappcenterportal.inuportal.domain.semester.dto.SemesterResponseDto;
 import kr.inuappcenterportal.inuportal.domain.semester.service.SemesterService;
+import kr.inuappcenterportal.inuportal.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,17 @@ public class SemesterController implements SemesterApiSpecification {
     public ResponseEntity<List<SemesterResponseDto>> getValidSemesters() {
         List<SemesterResponseDto> semesters = semesterService.getValidSemesters();
         return ResponseEntity.ok(semesters);
+    }
+
+    /**
+     * 학기 동기화 컨트롤러 (관리자 전용)
+     */
+    @PostMapping("/sync")
+    public ResponseEntity<ResponseDto<Void>> syncSemester() {
+        semesterService.syncSemestersByYear();
+
+        return ResponseEntity.ok(
+                ResponseDto.of(null, "학기 동기화 성공")
+        );
     }
 }
