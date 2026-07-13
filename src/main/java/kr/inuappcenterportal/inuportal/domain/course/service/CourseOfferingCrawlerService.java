525487964873397fe1f9d7cef0990088030d065d
siillvergun <duejql03@naver.com>
2026-07-13 23:28:44 +0900

    refactor: CourseOfferingCrawlerService의 메서드를 CourseGuideFileFetcher로 이동 및 책임분리

---
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/service/CourseOfferingCrawlerService.java
@@ -0,0 +1,13 @@
+package kr.inuappcenterportal.inuportal.domain.course.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CourseOfferingCrawlerService {
+
+
+}
