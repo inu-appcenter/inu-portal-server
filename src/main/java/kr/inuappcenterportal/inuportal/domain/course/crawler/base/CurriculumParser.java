9c9359ab911c3cb3cfccc8bc51e4a4ba6eac5108
siillvergun <duejql03@naver.com>
2026-07-14 00:09:07 +0900

    docs: package 이름 오류

--- src/main/java/kr/inuappcenterportal/inuportal/domain/course/crawler/base/CurriculumParser.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/crawler/base/CurriculumParser.java
@@ -1,4 +1,4 @@
-package kr.inuappcenterportal.inuportal.domain.course.crawler;
+package kr.inuappcenterportal.inuportal.domain.course.crawler.base;
 
 import kr.inuappcenterportal.inuportal.domain.course.dto.CurriculumItemDto;
 import lombok.extern.slf4j.Slf4j;
