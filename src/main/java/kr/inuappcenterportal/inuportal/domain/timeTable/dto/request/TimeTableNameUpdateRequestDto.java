f4e0e60c3088e9c4b605f64ebdbc60d4efd50840
siillvergun <duejql03@naver.com>
2026-07-17 00:30:37 +0900

    feat: dto 검증 어노테이션 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/dto/request/TimeTableNameUpdateRequestDto.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/dto/request/TimeTableNameUpdateRequestDto.java
@@ -1,6 +1,9 @@
 package kr.inuappcenterportal.inuportal.domain.timeTable.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record TimeTableNameUpdateRequestDto(
+        @NotBlank
         String timeTableName
 ) {
 }
--- src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/dto/request/TimeTableVisibilityUpdateRequestDto.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/dto/request/TimeTableVisibilityUpdateRequestDto.java
@@ -1,8 +1,10 @@
 package kr.inuappcenterportal.inuportal.domain.timeTable.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import kr.inuappcenterportal.inuportal.domain.timeTable.enums.Visibility;
 
 public record TimeTableVisibilityUpdateRequestDto(
+        @NotNull
         Visibility visibility
 ) {
 }
