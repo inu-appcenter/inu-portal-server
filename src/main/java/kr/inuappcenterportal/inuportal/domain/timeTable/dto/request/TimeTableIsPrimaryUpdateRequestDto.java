76e470eb338ea8b031468ec5ee168e42e690d07d
siillvergun <duejql03@naver.com>
2026-07-16 21:22:12 +0900

    feat: 수정 전용 dto 개발

---
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/dto/request/TimeTableIsPrimaryUpdateRequestDto.java
@@ -0,0 +1,6 @@
+package kr.inuappcenterportal.inuportal.domain.timeTable.dto.request;
+
+public record TimeTableIsPrimaryUpdateRequestDto(
+        boolean isPrimary
+) {
+}
---
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/dto/request/TimeTableNameUpdateRequestDto.java
@@ -0,0 +1,6 @@
+package kr.inuappcenterportal.inuportal.domain.timeTable.dto.request;
+
+public record TimeTableNameUpdateRequestDto(
+        String timeTableName
+) {
+}
