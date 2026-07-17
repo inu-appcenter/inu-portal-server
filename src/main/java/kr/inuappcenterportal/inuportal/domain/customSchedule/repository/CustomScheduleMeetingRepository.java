dc908cb18c3b73f457b2c064b940673cdb159242
siillvergun <duejql03@naver.com>
2026-07-18 01:33:43 +0900

    feat: service에 필요한 repository 메서드 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/repository/CustomScheduleMeetingRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/repository/CustomScheduleMeetingRepository.java
@@ -4,4 +4,7 @@ import kr.inuappcenterportal.inuportal.domain.customSchedule.model.CustomSchedul
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomScheduleMeetingRepository extends JpaRepository<CustomScheduleMeeting, Long> {
+    void deleteAllByCustomScheduleId(Long customScheduleId);
+
+    Long countByCustomScheduleId(Long customScheduleId);
 }
