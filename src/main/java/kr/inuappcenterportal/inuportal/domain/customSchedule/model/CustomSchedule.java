d5b255f384580cefe8a3961714b6d5e5b77dd492
siillvergun <duejql03@naver.com>
2026-07-18 03:04:12 +0900

    refactor: 커스텀일정은 시간표요소에 속하므로 불필요한 필드 제거

--- src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/model/CustomSchedule.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/model/CustomSchedule.java
@@ -1,8 +1,6 @@
 package kr.inuappcenterportal.inuportal.domain.customSchedule.model;
 
 import jakarta.persistence.*;
-import kr.inuappcenterportal.inuportal.domain.member.model.Member;
-import kr.inuappcenterportal.inuportal.domain.semester.model.Semester;
 import kr.inuappcenterportal.inuportal.global.model.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,34 +16,18 @@ public class CustomSchedule extends BaseTimeEntity {
     @Column(name = "custom_schedule_id", nullable = false)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "semester_id", nullable = false)
-    private Semester semester;
-
     @Column(nullable = false)
     private String title;
 
-    private CustomSchedule(
-            Member member,
-            Semester semester,
-            String title
-    ) {
-        this.member = member;
-        this.semester = semester;
-        this.title = title;
+    private CustomSchedule(String title) {
+        setCustomScheduleTitle(title);
     }
 
     // 정적 팩토리 메서드
     public static CustomSchedule create(
-            Member member,
-            Semester semester,
             String title
     ) {
-        return new CustomSchedule(member, semester, title);
+        return new CustomSchedule(title);
     }
 
     public void setCustomScheduleTitle(String updateTitle) {
