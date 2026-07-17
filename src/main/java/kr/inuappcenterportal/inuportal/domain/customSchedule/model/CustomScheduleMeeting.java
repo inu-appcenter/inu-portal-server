04e60f222a6868d85845eea8cd3bd46b1db264bc
siillvergun <duejql03@naver.com>
2026-07-17 21:53:38 +0900

    feat: 커스텀일정 시간 엔티티 개발

---
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/model/CustomScheduleMeeting.java
@@ -0,0 +1,66 @@
+package kr.inuappcenterportal.inuportal.domain.customSchedule.model;
+
+import jakarta.persistence.*;
+import kr.inuappcenterportal.inuportal.domain.course.enums.DayOfWeek;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CustomScheduleMeeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "custom_schedule_meeting_id", nullable = false)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "custom_schedule_id", nullable = false)
+    CustomSchedule customSchedule;
+
+    @Column(nullable = false)
+    String location;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    DayOfWeek day;
+
+    @Column(nullable = false)
+    LocalTime startTime;
+
+    @Column(nullable = false)
+    LocalTime endTime;
+
+    private CustomScheduleMeeting(
+            CustomSchedule customSchedule,
+            String location,
+            DayOfWeek day,
+            LocalTime startTime,
+            LocalTime endTime
+    ) {
+        this.customSchedule = customSchedule;
+        this.location = location;
+        this.day = day;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    // 정적 팩토리 메서드
+    public static CustomScheduleMeeting create(
+            CustomSchedule customSchedule,
+            String location,
+            DayOfWeek day,
+            LocalTime startTime,
+            LocalTime endTime
+    ) {
+        if (!startTime.isBefore(endTime)) {
+            throw new IllegalArgumentException("시작 시간은 종료 시간보다 빨라야합니다.");
+        }
+
+        return new CustomScheduleMeeting(customSchedule, location, day, startTime, endTime);
+    }
+}
