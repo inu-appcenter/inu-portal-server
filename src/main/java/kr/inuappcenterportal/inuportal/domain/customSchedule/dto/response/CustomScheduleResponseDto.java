3ef372ae6cbf2ca734e0271be6cf8f03dfc53417
siillvergun <duejql03@naver.com>
2026-07-17 21:54:38 +0900

    feat: 커스텀일정 응답, 요청 dto 개발

---
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/dto/response/CustomScheduleResponseDto.java
@@ -0,0 +1,25 @@
+package kr.inuappcenterportal.inuportal.domain.customSchedule.dto.response;
+
+import kr.inuappcenterportal.inuportal.domain.customSchedule.model.CustomSchedule;
+import kr.inuappcenterportal.inuportal.domain.customSchedule.model.CustomScheduleMeeting;
+
+import java.util.List;
+
+public record CustomScheduleResponseDto(
+        Long id,
+        String title,
+        List<CustomScheduleMeetingResponseDto> meetings
+) {
+
+    public static CustomScheduleResponseDto from(
+            CustomSchedule customSchedule,
+            List<CustomScheduleMeeting> meetings) {
+        return new CustomScheduleResponseDto(
+                customSchedule.getId(),
+                customSchedule.getTitle(),
+                meetings.stream()
+                        .map(CustomScheduleMeetingResponseDto::from)
+                        .toList()
+        );
+    }
+}
