a7ac7ad1d3a9db30400c2a25b2b4ed10572c8950
siillvergun <duejql03@naver.com>
2026-07-17 21:53:57 +0900

    feat: 커스텀일정 관련 레포지토리 개발

---
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/repository/CustomScheduleMeetingRepository.java
@@ -0,0 +1,7 @@
+package kr.inuappcenterportal.inuportal.domain.customSchedule.repository;
+
+import kr.inuappcenterportal.inuportal.domain.customSchedule.model.CustomScheduleMeeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomScheduleMeetingRepository extends JpaRepository<CustomScheduleMeeting, Long> {
+}
