d8943ce50a39e401652c7dbae010c464ff1b08c6
siillvergun <duejql03@naver.com>
2026-07-18 03:15:44 +0900

    docs: 스웨서 스키마 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/dto/response/CustomScheduleResponseDto.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/dto/response/CustomScheduleResponseDto.java
@@ -1,13 +1,20 @@
 package kr.inuappcenterportal.inuportal.domain.customSchedule.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import kr.inuappcenterportal.inuportal.domain.customSchedule.model.CustomSchedule;
 import kr.inuappcenterportal.inuportal.domain.customSchedule.model.CustomScheduleMeeting;
 
 import java.util.List;
 
+@Schema(description = "커스텀일정 응답 DTO")
 public record CustomScheduleResponseDto(
+        @Schema(description = "커스텀일정 id", example = "1")
         Long id,
+
+        @Schema(description = "커스텀일정 제목", example = "알바")
         String title,
+
+        @Schema(description = "커스텀일정 시간 목록")
         List<CustomScheduleMeetingResponseDto> meetings
 ) {
 
