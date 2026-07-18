3be405705f83fc220148e9f808a96760b095f5bd
siillvergun <duejql03@naver.com>
2026-07-19 00:04:17 +0900

    docs: 파일 위치 이동

--- src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/repository/TimeTableItemRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/timeTable/repository/TimeTableItemRepository.java
@@ -3,10 +3,11 @@ package kr.inuappcenterportal.inuportal.domain.timeTable.repository;
 import kr.inuappcenterportal.inuportal.domain.timeTable.model.TimeTableItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TimeTableItemRepository extends JpaRepository<TimeTableItem, Long> {
-    void deleteAllByTimeTableId(Long timeTableId);
-
     Optional<TimeTableItem> findByCustomScheduleId(Long customScheduleId);
+
+    List<TimeTableItem> findAllByTimeTableId(Long timeTableId);
 }
