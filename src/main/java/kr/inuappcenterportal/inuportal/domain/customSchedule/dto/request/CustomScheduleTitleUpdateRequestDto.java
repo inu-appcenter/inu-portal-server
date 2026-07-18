1b0296082f84d841faa8d42185f90ef9412987a8
siillvergun <duejql03@naver.com>
2026-07-18 16:15:19 +0900

    feat: 잘못된 설계로 인한 삭제

--- src/main/java/kr/inuappcenterportal/inuportal/domain/customSchedule/dto/request/CustomScheduleTitleUpdateRequestDto.java
+++
@@ -1,8 +0,0 @@
-package kr.inuappcenterportal.inuportal.domain.customSchedule.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record CustomScheduleTitleUpdateRequestDto(
-        @NotBlank String title
-) {
-}
