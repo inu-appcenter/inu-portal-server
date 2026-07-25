9752c8623637a1b01bd9bf7fcdef736cd20f6141
siillvergun <duejql03@naver.com>
2026-07-25 14:52:31 +0900

    feat: 두 회원이 친구관계인지 확인하는 메서드 개발

--- src/main/java/kr/inuappcenterportal/inuportal/domain/member/repository/FriendRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/member/repository/FriendRepository.java
@@ -1,17 +1,39 @@
 package kr.inuappcenterportal.inuportal.domain.member.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import kr.inuappcenterportal.inuportal.domain.member.enums.FriendStatus;
 import kr.inuappcenterportal.inuportal.domain.member.model.Friend;
 import kr.inuappcenterportal.inuportal.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
     Optional<Friend> findByRequesterAndReceiver(Member requester, Member receiver);
+
     List<Friend> findAllByRequesterAndStatus(Member requester, FriendStatus status);
+
     List<Friend> findAllByReceiverAndStatus(Member receiver, FriendStatus status);
+
     boolean existsByRequesterAndReceiverAndStatus(Member requester, Member receiver, FriendStatus status);
+
     boolean existsByRequesterAndReceiver(Member requester, Member receiver);
+
+    @Query("""
+            select case when count(f) > 0 then true else false end
+            from Friend f
+            where f.status = :status
+              and (
+                (f.requester.id = :memberId and f.receiver.id = :targetMemberId)
+                or
+                (f.requester.id = :targetMemberId and f.receiver.id = :memberId)
+              )
+            """)
+    boolean existsFriendship(
+            @Param("memberId") Long memberId,
+            @Param("targetMemberId") Long targetMemberId,
+            @Param("status") FriendStatus status
+    );
 }
