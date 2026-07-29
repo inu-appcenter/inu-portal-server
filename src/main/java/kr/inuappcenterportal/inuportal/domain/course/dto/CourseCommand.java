f93da7e0e4fb414854823a412e83db31b60ee537
siillvergun <duejql03@naver.com>
2026-07-29 17:25:08 +0900

    feat: 개설 강의 전용 내부 객체 생성

---
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/dto/CourseCommand.java
@@ -0,0 +1,16 @@
+package kr.inuappcenterportal.inuportal.domain.course.dto;
+
+import kr.inuappcenterportal.inuportal.domain.course.enums.CompletionDivision;
+import kr.inuappcenterportal.inuportal.domain.course.enums.TargetGrade;
+import kr.inuappcenterportal.inuportal.domain.department.enums.Department;
+
+public record CourseCommand(
+        Long courseId,
+        String courseCode,
+        String title,
+        Department department,
+        TargetGrade targetGrade,
+        CompletionDivision completionDivision,
+        String credit
+) {
+}
