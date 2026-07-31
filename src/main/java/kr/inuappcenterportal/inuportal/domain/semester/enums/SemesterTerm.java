58790e6fe42a0aba2b5a50c4fe0fc34514fc5c3d
siillvergun <duejql03@naver.com>
2026-07-31 15:14:40 +0900

    feat: 개설 강의 api 응답에 맞게 매핑 로직 개발

--- src/main/java/kr/inuappcenterportal/inuportal/domain/semester/enums/SemesterTerm.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/semester/enums/SemesterTerm.java
@@ -1,5 +1,7 @@
 package kr.inuappcenterportal.inuportal.domain.semester.enums;
 
+import kr.inuappcenterportal.inuportal.global.exception.ex.MyErrorCode;
+import kr.inuappcenterportal.inuportal.global.exception.ex.MyException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,9 +9,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SemesterTerm {
     FIRST("1학기"),
-    SUMMER("여름학기"),
+    SUMMER("여름계절학기"),
     SECOND("2학기"),
-    WINTER("겨울학기");
+    WINTER("겨울계절학기");
 
     private final String displayName;
+
+    public static SemesterTerm mapToTermCode(String termCode) {
+        if (termCode.equals("10")) {
+            return FIRST;
+        } else if (termCode.equals("20")) {
+            return SECOND;
+        } else if (termCode.equals("30")) {
+            return SUMMER;
+        } else if (termCode.equals("40")) {
+            return WINTER;
+        } else {
+            throw new MyException(MyErrorCode.SEMESTER_NOT_FOUND);
+        }
+    }
 }
