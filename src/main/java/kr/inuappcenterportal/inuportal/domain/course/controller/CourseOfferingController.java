46bbdaa7bd69da2038b68fd022035d45b4269549
siillvergun <duejql03@naver.com>
2026-08-01 03:43:37 +0900

    feat: 학기 기준 개설 강의 조회 컨트롤러 개발

--- src/main/java/kr/inuappcenterportal/inuportal/domain/course/controller/CourseOfferingController.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/controller/CourseOfferingController.java
@@ -1,19 +1,24 @@
 package kr.inuappcenterportal.inuportal.domain.course.controller;
 
+import kr.inuappcenterportal.inuportal.domain.course.dto.courseOffering.CourseOfferingResponseDto;
+import kr.inuappcenterportal.inuportal.domain.course.service.CourseOfferingService;
 import kr.inuappcenterportal.inuportal.domain.course.service.CourseOfferingSyncService;
+import kr.inuappcenterportal.inuportal.domain.semester.enums.SemesterTerm;
 import kr.inuappcenterportal.inuportal.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/course-offerings")
-public class CourseOfferingController {
+public class CourseOfferingController implements CourseOfferingApiSpecification {
+
     private final CourseOfferingSyncService courseOfferingSyncService;
+    private final CourseOfferingService courseOfferingService;
 
     @PostMapping("/sync")
     public ResponseEntity<ResponseDto<Void>> syncCourseOffering(
@@ -22,5 +27,16 @@ public class CourseOfferingController {
     ) {
         courseOfferingSyncService.syncCourseWithSchoolApi(year, modDate);
         return ResponseEntity.ok(ResponseDto.of(null, "개설 강의 동기화 성공"));
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseDto<Page<CourseOfferingResponseDto>>> getCourseOfferings(
+            @RequestParam Integer year,
+            @RequestParam SemesterTerm term,
+            @PageableDefault(size = 20) Pageable pageable
+    ) {
+        return ResponseEntity.ok(
+                ResponseDto.of(courseOfferingService.getCourseOfferings(year, term, pageable), "개설 강의 목록 조회 성공")
+        );
     }
 }
