b6c1a04d56c9c9658e03266b07a75dd412b0761f
siillvergun <duejql03@naver.com>
2026-08-03 16:35:19 +0900

    feat: Querydsl config 개발

---
+++ src/main/java/kr/inuappcenterportal/inuportal/global/config/QuerydslConfig.java
@@ -0,0 +1,15 @@
+package kr.inuappcenterportal.inuportal.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager manager) {
+        return new JPAQueryFactory(manager);
+    }
+}
