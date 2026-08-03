ea3e8f0c8588a83a4c515142897ca43c027eccaf
siillvergun <duejql03@naver.com>
2026-08-03 16:36:15 +0900

    feat: Querydsl을 위한 repository와 그 구현체 개발

--- src/main/java/kr/inuappcenterportal/inuportal/domain/course/repository/CourseOfferingRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/repository/CourseOfferingRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CourseOfferingRepository extends JpaRepository<CourseOffering, Long> {
+public interface CourseOfferingRepository extends JpaRepository<CourseOffering, Long>, CourseOfferingRepositoryCustom {
 
     Optional<CourseOffering> findBySemesterIdAndSubjectNumber(Long semesterId, String subjectNumber);
 
