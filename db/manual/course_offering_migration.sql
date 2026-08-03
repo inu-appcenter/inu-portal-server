c19604feb21e965cec2f59cf32327e5c589d1f90
siillvergun <duejql03@naver.com>
2026-08-03 12:36:51 +0900

    docs: courseOffering 마이그래이션 sql파일 추가

---
+++ db/manual/course_offering_migration.sql
@@ -0,0 +1,17 @@
+  DELETE FROM course_meeting;
+
+  DELETE FROM timetable_item
+  WHERE course_offering_id IS NOT NULL;
+
+  DELETE FROM course_offering;
+
+  ALTER TABLE course_offering
+      DROP COLUMN method,
+      DROP COLUMN target_department,
+      ADD COLUMN cnctr_isu_name varchar(255) NULL,
+      ADD COLUMN dept_name varchar(255) NULL,
+      ADD COLUMN college_name varchar(255) NULL,
+      ADD COLUMN isu_fld_name varchar(255) NULL,
+      ADD COLUMN isu_name varchar(255) NULL,
+      ADD COLUMN ssup_type_name varchar(255) NULL,
+      ADD COLUMN credit varchar(255) NULL;
