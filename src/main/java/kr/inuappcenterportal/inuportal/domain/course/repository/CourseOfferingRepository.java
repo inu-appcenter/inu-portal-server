eab3f24c60e98b2fed11284ab469c6747f70f06d
siillvergun <duejql03@naver.com>
2026-08-07 01:34:09 +0900

    feat: 정렬된 응답을 보내도록 Querydsl 안에 정렬 기준 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/course/repository/CourseOfferingRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/repository/CourseOfferingRepository.java
@@ -1,9 +1,6 @@
 package kr.inuappcenterportal.inuportal.domain.course.repository;
 
 import kr.inuappcenterportal.inuportal.domain.course.model.CourseOffering;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,7 +8,4 @@ import java.util.Optional;
 public interface CourseOfferingRepository extends JpaRepository<CourseOffering, Long>, CourseOfferingRepositoryCustom {
 
     Optional<CourseOffering> findBySemesterIdAndSubjectNumber(Long semesterId, String subjectNumber);
-
-    @EntityGraph(attributePaths = {"course", "semester"})
-    Page<CourseOffering> findAllBySemesterId(Long semesterId, Pageable pageable);
 }
