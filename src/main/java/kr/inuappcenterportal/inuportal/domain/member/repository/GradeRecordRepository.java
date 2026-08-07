f98c48458fdcf29982449a822a49fb740b39ff58
siillvergun <duejql03@naver.com>
2026-08-07 18:34:19 +0900

    feat: 성적 개별 삭제를 위한 레포지토리 메서드 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/member/repository/GradeRecordRepository.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/member/repository/GradeRecordRepository.java
@@ -4,9 +4,12 @@ import kr.inuappcenterportal.inuportal.domain.member.model.GradeRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GradeRecordRepository extends JpaRepository<GradeRecord, Long> {
     List<GradeRecord> findAllByMemberIdAndSemesterId(Long memberId, Long semesterId);
 
     void deleteAllByMemberIdAndSemesterId(Long memberId, Long semesterId);
+
+    Optional<GradeRecord> findByIdAndMemberId(Long id, Long memberId);
 }
