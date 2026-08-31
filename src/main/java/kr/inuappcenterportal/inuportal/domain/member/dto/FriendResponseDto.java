bcfbc0144d4d726f9a2ace2aa81c100377c91c87
KyoungMin Choi <ckm0728wash@gmail.com>
2026-08-31 18:58:54 +0900

    feat: 친구추가 API응답에 학과 필드 추가

--- src/main/java/kr/inuappcenterportal/inuportal/domain/member/dto/FriendResponseDto.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/member/dto/FriendResponseDto.java
@@ -1,12 +1,14 @@
 package kr.inuappcenterportal.inuportal.domain.member.dto;
 
+import kr.inuappcenterportal.inuportal.domain.department.enums.Department;
 import lombok.Builder;
 
 /**
  * @param friendId friend 엔티티의 ID
+ * @param department 친구의 학과
  */
 public record FriendResponseDto(Long friendId, Long friendMemberId, String nickname, String studentId, Long fireId,
-                                String friendAlias) {
+                                String friendAlias, Department department) {
     @Builder
     public FriendResponseDto {
     }
