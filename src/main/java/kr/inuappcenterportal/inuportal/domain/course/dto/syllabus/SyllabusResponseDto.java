69373a2ab7d4a99c3cf64158ffaf94f7f60c80d5
siillvergun <duejql03@naver.com>
2026-09-10 17:12:35 +0900

    feat: 필드 제거

--- src/main/java/kr/inuappcenterportal/inuportal/domain/course/dto/syllabus/SyllabusResponseDto.java
+++ src/main/java/kr/inuappcenterportal/inuportal/domain/course/dto/syllabus/SyllabusResponseDto.java
@@ -1,22 +1,17 @@
 package kr.inuappcenterportal.inuportal.domain.course.dto.syllabus;
 
 import kr.inuappcenterportal.inuportal.domain.course.dto.SyllabusContent;
-import kr.inuappcenterportal.inuportal.domain.course.model.CourseOffering;
 import kr.inuappcenterportal.inuportal.domain.course.model.Syllabus;
 
 public record SyllabusResponseDto(
         Long id,
-        CourseOffering courseOffering,
-        String subjectCode,
-        String professor,
+        Long courseOfferingId,
         SyllabusContent content
 ) {
     public static SyllabusResponseDto from(Syllabus syllabus) {
         return new SyllabusResponseDto(
                 syllabus.getId(),
-                syllabus.getCourseOffering(),
-                syllabus.getSubjectCode(),
-                syllabus.getProfessor(),
+                syllabus.getCourseOffering().getId(),
                 syllabus.getContent()
         );
     }
